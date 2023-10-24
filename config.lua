34a6e2a6d04463e430784ddcfc5c98347b1d1966
DFNZ Scripts <122015772+dFineZ@users.noreply.github.com>
2023-10-24 07:20:06 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+Config.ChangeFirstNamePrice = 2500 -- how much should it cost to change the fistname
+Config.ChangeLastNamePrice = 1000 -- how much should it cost to change the lastname
+
+Config.EnableBlip = true -- activate Blip? true or false
+Config.Blip = { sprite = 409, colour = 44, size = 1.0, name = 'Judge'}
+Config.EnablePed = true
+Config.Location = { x = 1135.8882, y = -784.9349, z = 57.5987, rot = 319.8426, model = 'cs_barry', drawSprite = false }
+
+Config.NotifyDuration = 5000
+Config.NotifyPosition = 'top'
+Config.IconColor = '#FF6000'
+
+Config.WebhookURL = 'Your Webhook' -- Enter your Webhook URL here
+Config.WebhookColor = 16735488 -- Enter your Webhook Color here you can use this Side https://www.mathsisfun.com/hexadecimal-decimal-colors.html (it needs to be in decimal format)
+
