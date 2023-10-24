1debdac3f9f092d3538702147e72e58d955ab1fb
DFNZ Scripts <122015772+dFineZ@users.noreply.github.com>
2023-10-24 07:20:34 +0200

    update v2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,11 +4,15 @@ lua54 'yes'
 
 author 'DFNZ'
 description 'change your name easy ingame'
-version '1.0.0'
+version '1.0.5'
 
 shared_script {
     '@es_extended/imports.lua',
     '@ox_lib/init.lua'
+}
+
+files {
+    "locales/*.json"
 }
 
 client_scripts {
